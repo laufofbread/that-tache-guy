b3264fe9bfafc0c94d5094c4b922d0f89ef52394
Laura McCartney <hello@lauramccartney.co.uk>
2014-11-07 00:42:48 +0000

    Add reviews to header

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -8,7 +8,7 @@
   <title>Guy Combes</title>
   <meta name="description" content="<?php echo $site->description()->html() ?>">
   <meta name="keywords" content="<?php echo $site->keywords()->html() ?>">
-
+    <link href='http://fonts.googleapis.com/css?family=Lobster+Two|Oswald:400,300' rel='stylesheet' type='text/css'>
   <?php echo css('assets/fonts/stylesheet.css') ?>
   <?php echo css('assets/css/style.css') ?>
 
@@ -16,5 +16,9 @@
 <body>
 
   <div class="header-container">
-      <img class="logo" src="assets/images/header.png" width="1800" height="160" alt="Header">
+      <h1 class="reviews"><i>Surreal and</i><br>exquisite
+<span>Broadway Baby</span></h1>
+      <img class="logo" src="assets/images/logo.png" width="1800" height="160" alt="Guy Combes">
+      <h1 class="reviews">Hilarious!
+<span>Such Small Portions</span></h1>
   </div>

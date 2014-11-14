521b6be49e8e78cdbf8a3592bf94b08d1d4801af
Laura McCartney <hello@lauramccartney.co.uk>
2014-11-14 20:56:21 +0000

    Add back home content file because of 500 error

---
+++ site/templates/events.php
@@ -0,0 +1,12 @@
+<?php snippet('header') ?>
+
+  <main class="main" role="main">
+
+    <div class="text">
+      <h1>Come see me!</h1>
+      <?php snippet('events') ?>
+    </div>
+
+  </main>
+
+<?php snippet('footer') ?>
